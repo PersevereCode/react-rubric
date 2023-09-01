baa82820c51b4afeadae42d3da332e7588538ec3
kylexrose@gmail.com <kylexrose@gmail.com>
2023-09-01 15:26:58 -0400

    Scoring object functionality started

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,32 +6,44 @@ import FormLabel from '@mui/material/FormLabel';
 import { queryByTestId } from '@testing-library/react';
 import Question from './Question';
 
-function Section({section}) {
+function Section({section, gradeObj, setGradeObj}) {
     const {sectionTitle, questions} = section;
 
     const [sectionTotal, setSectionTotal] = useState(0);
+    const [sectionScoringObj, setSectionScoringObj] = useState({});
     const [sectionScore, setSectionScore] = useState(0);
     
     useEffect(() => {
         let total = 0;
+        let scoreObj = {};
         questions.map(question => {
             if(question.type === 'boolean'){
-            total += question.score;
+                total += question.score;
+                scoreObj[question.message || question.criteria] = 0
             }else if(question.type === 'likert'){
+                //TODO rewrite so more forgiving on order of likert prompts
                 total += question.options[question.options.length - 1].score;
+                scoreObj[question.options[0].description] = 0;
             }
         })
         setSectionTotal(total)
+        setSectionScoringObj(scoreObj)
     }, [])
+
 
     const renderForm = () => {
         return(
             questions.map(question => {
-                return <div><Question question = {question} /></div>;
+                return <div><Question question = {question} 
+                setSectionScoringObj = {setSectionScoringObj}
+                sectionScoringObj = {sectionScoringObj}/></div>;
             })
         )
     }
 
+    // const computeScore = (e) = {
+
+    // }
     
   return (
     <div className='sectionCardContainer'>
