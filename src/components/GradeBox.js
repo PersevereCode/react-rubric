fb355012f085f3a4835fa81da65e2a19169acc4a
kylexrose@gmail.com <kylexrose@gmail.com>
2023-10-03 12:28:38 -0400

    likert gradebox functionality working

--- src/components/GradeBox.js
+++ src/components/GradeBox.js
@@ -4,8 +4,10 @@ import Button from '@mui/material/Button';
 import GradeBoxSection from './GradeBoxSection';
 import { useEffect, useState } from 'react';
 
-function GradeBox({gradeObj, totalPossible, sectionTotals}) {
+function GradeBox({gradeObj, totalPossible, sectionTotals, RUBRIC}) {
   const [total, setTotal] = useState(0)
+  const [grade, setGrade] = useState('F')
+
 
 
   const totalScore = () => {
@@ -15,7 +17,19 @@ function GradeBox({gradeObj, totalPossible, sectionTotals}) {
         sum += gradeObj[section][question]
       }
     }
+    setGrade(returnGrade(sum/ totalPossible))
     setTotal(sum)
+  }
+
+  const returnGrade = (num) => {
+    switch(true){
+      case num >= .9 : return 'A';
+      case num >= .8 : return 'B';
+      case num >= .7 : return 'C';
+      case num >= .6 : return 'D';
+      case num < .6 : return 'F';
+      default: return;
+    }
   }
 
   const renderGradeBoxSections = () => {
@@ -24,7 +38,8 @@ function GradeBox({gradeObj, totalPossible, sectionTotals}) {
         return <GradeBoxSection key={sectionTitle}
                                 section = {gradeObj[sectionTitle]}
                                 sectionTitle = {sectionTitle}
-                                totalPossibleScoreInSection = {sectionTotals[sectionTitle]}/>
+                                totalPossibleScoreInSection = {sectionTotals[sectionTitle]}
+                                rubricSection = {RUBRIC.sections.find((section => section.sectionTitle === sectionTitle))}/>
       })
     )
   }
@@ -36,7 +51,7 @@ function GradeBox({gradeObj, totalPossible, sectionTotals}) {
   return (
     <div className='sectionCardContainer'>
         <Card sx={{ minWidth: 275 }} className='card'>
-            <h2 className="sectionHead">Recommended Grade: F</h2>
+            <h2 className="sectionHead">Recommended Grade: {grade}</h2>
             <h2 className="sectionHead">Total: ({total}/{totalPossible})</h2>
             <Card sx={{ minWidth: 275, backgroundColor: '#d5d9de' }} className='innerCard'>
               {renderGradeBoxSections()}
--- src/components/GradeBoxSection.js
+++ src/components/GradeBoxSection.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Button } from '@mui/material'
 
-function GradeBoxSection({section, sectionTitle, totalPossibleScoreInSection}) {
+function GradeBoxSection({section, sectionTitle, totalPossibleScoreInSection, rubricSection}) {
     const [totalScored, setTotalScored] = useState(0)
-
     const totalScore = () => {
         let total = 0;
         for(let key in section){
-            
             total += section[key]
         }
         setTotalScored(total)
@@ -22,23 +20,45 @@ function GradeBoxSection({section, sectionTitle, totalPossibleScoreInSection}) {
             <div className='gradeBox'>
                 {Object.keys(section).map(criteria =>{
                     if(!section[criteria]){
-                        return <p key= {criteria}>* {criteria}</p>
-                    }else if(section[criteria] ){
+                        return <p className= {sectionTitle.split(' ').join('') +' groupCopy'} key= {criteria}>* {criteria}</p>
+                    }else{
+                        let currentRubricQuestion = rubricSection.questions.find((question)=>{
+                            let found = question.options ? question.options.find((option)=> option.description === criteria) : [];
+                            return found ? question : undefined
+                        })
+                        let isHighestPossible = true;
+                        for(let option of currentRubricQuestion.options){
+                            if(option.score > section[criteria]){
+                                isHighestPossible = false;
+                            }
+                        }
+                        if(!isHighestPossible){
+                        return <p className= {sectionTitle.split(' ').join('') +' groupCopy'} key= {criteria}>* {criteria}</p>
+                        }
+                    }
+                        
                         
                     }
-                })}
+                )}
             </div>
         )
     }
 
-    const copyText = () =>{
-
+    const copyText = () => {
+        const className = sectionTitle.split(' ').join('')
+        const arrOfCriteria = document.querySelectorAll(`.${className}.groupCopy`)
+        let fullCopy = '';
+        for(let element of arrOfCriteria){
+            fullCopy += element.innerText + '\n'
+        }
+        navigator.clipboard.writeText(fullCopy)
     }
+
   return (
     <div>
         <h3>{sectionTitle}  {totalScored}/{totalPossibleScoreInSection}</h3>
         {renderCriteria()}
-        <Button>Copy to Clipboard</Button>
+        <Button onClick={copyText}>Copy to Clipboard</Button>
     </div>
   )
 }
